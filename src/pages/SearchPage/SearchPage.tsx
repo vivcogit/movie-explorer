0b7a72e9dc6361c2df4d4e85171fc36195123b35
vivcogit <vivcogit@gmail.com>
2020-02-13 16:15:15 +0300

    fix: Missed file was added to repo

---
+++ src/pages/SearchPage/SearchPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function SearchPage() {
+    return (
+        <div className="SearchPage">
+            <div className="SearchPage-InputContainer">
+                <input className="SearchPage-Input" type="text" />
+            </div>
+        </div>
+    );
+}
+
+export { SearchPage };
